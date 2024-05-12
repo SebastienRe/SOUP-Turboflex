946db7c894befbdea60be6dffa1fc34257061e33
NicolasUrban2 <nclsurban@gmail.com>
2024-05-12 12:30:50 +0200

    Add the record button to the main activity

--- client/app/src/main/java/com/example/soupturboflex/MainActivity.java
+++ client/app/src/main/java/com/example/soupturboflex/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.soupturboflex;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,7 +13,13 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    private Button recordButton;
 
+    /**
+     * Reason for SuppressLint : I am cold-hearted and don't care about making the world a
+     * better place for blind people.
+     */
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +29,20 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        recordButton = findViewById(R.id.record_button);
+
+        recordButton.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                if(motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
+                    System.out.println("Button pressin");
+                } else if(motionEvent.getAction() == MotionEvent.ACTION_UP) {
+                    System.out.println("Button released");
+                }
+                return true;
+            }
         });
     }
 }
